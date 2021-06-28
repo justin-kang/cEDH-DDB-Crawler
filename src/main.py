80de597f4b38a653644fa2e541332ddbcde4935c
Justin Kang <justin.kang23@gmail.com>
2021-06-28 05:27:10 -0500

    update parser to account for companions

--- src/main.py
+++ src/main.py
@@ -5,9 +5,13 @@ if __name__ == '__main__':
     crawler = Crawler('https://cedh-decklist-database.com/')
     crawler.run()
     for link in crawler.links:
-        #parser = None
         if 'moxfield' in link:
             parser = MoxfieldParser(link)
         elif 'archidekt' in link:
-            parser = ArchidecktParser(link)
-        parser.run()
+            parser = ArchidektParser(link)
+        # sometimes doesn't read the decklist, so loop until it does
+        while not parser.decklist:
+            parser.run()
+        # TODO
+    # make sure to close the browser to avoid mem leaks
+    driver.quit()
--- src/parser.py
+++ src/parser.py
@@ -2,7 +2,7 @@ import logging
 # for splitting text with regex
 import re
 from bs4 import BeautifulSoup as bs
-# used to execute JS for webcrawling
+# used to execute JS for webcrawling, needed for moxfield
 from selenium.webdriver import Firefox, firefox
 
 options = firefox.options.Options()
@@ -35,21 +35,28 @@ class MoxfieldParser(Parser):
     def _parse_decklist(self):
         soup = super()._parse_decklist()
         types = {'Commander','Creatures','Planeswalkers','Enchantments',
-                'Artifacts','Sorceries','Instants','Lands'}
+                'Artifacts','Sorceries','Instants','Lands','Sideboard'}
         for table in soup.find_all('table'):
             if table.has_attr('class'):
                 if 'table-deck' in table['class']:
                     # split up card name from type
                     text = table.text.strip().split(maxsplit=1)
-                    # ignore non-type categories, like sideboard/considering
+                    # ignore non-type categories like considering
                     if text[0] not in types:
                         continue
-                    # use regex to get all the individual card names
+                    # use regex to filter card names
                     cards = text[1].split(')',1)[1]
                     if 'Lands' in text[0]:
                         if 'mdfc' in text[1]:
                             cards = text[1].split('mdfc',1)[1]
-                    cards = re.split('\d+',cards)
+                    cards = re.split(r'[\d+]',cards)[1:]
+                    # check for companions in sideboard
+                    if 'Sideboard' in text[0]:
+                        for i,row in enumerate(table.tbody.find_all('tr')):
+                            if row.select('#companion-icon'):
+                                self.decklist.add(cards[i])
+                                break
+                        continue
                     for card in cards:
                         if card:
                             self.decklist.add(card)
